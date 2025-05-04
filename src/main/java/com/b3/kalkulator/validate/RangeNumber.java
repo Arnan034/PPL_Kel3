682a6e3de5489a927624f0f5b57e7e5736e6c700
Arnan <arnanda.prasatya.tif422@polban.ac.id>
2025-05-04 19:48:42 +0700

    merge test reporting group

--- src/main/java/com/b3/kalkulator/validate/RangeNumber.java
+++ src/main/java/com/b3/kalkulator/validate/RangeNumber.java
@@ -2,7 +2,7 @@ package com.b3.kalkulator.validate;
 
 public class RangeNumber {
     public boolean validateRange(int number) {
-        if (number < -32768 || number > 32767) {
+        if (number <= -32768 || number >= 32767) {
             return false;
         }
         return true;
